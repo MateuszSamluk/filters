b6627d93231e5882ebe3795095c23180d6de145c
MateuszSamluk <mat.sam00@wp.pl>
2021-05-17 22:51:34 +0200

    Dodaj pliki projektów.

---
+++ filters/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var ipAddress = context.HttpContext.Connection.RemoteIpAddress.ToString();  //Request.Host.ToString();
+            var result = (PageResult)context.Result;
+            result.ViewData["IpAddress"] = ipAddress;
+            await next.Invoke();
+        }
+    }
+}
---
+++ filters/Utils/CustomPageFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filters.Utils
+{
+    public class CustomPageFilter : IAsyncPageFilter
+    {
+        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            await Task.CompletedTask;
+        }
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            var ipAddress = context.HttpContext.Request.Host.ToString();
+            var result = (PageResult)context.Result;
+            //result.ViewData["IpAddress"] = ipAddress;
+            await next.Invoke();
+        }
+    }
+}
